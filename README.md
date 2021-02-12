05c578481459ca614a4270e44be23b049805aa3d
Tyler4Rock <78806615+EilLAnd21@users.noreply.github.com>
2021-02-12 15:22:02 -0800

    Uploaded Toolpost Meme Music Video
    dfdadasd

--- README.md
+++ README.md
@@ -1,17 +1,38 @@
-# YTPMVPlus
-YTPMV+ is a brand new rebuilt system of YTPMV based on the
-GTPitch revision 1.0, coded from the ground up. It allows for enhanced
-processing, improved pitch, accessible visual presets, easy masking,
-a better cookie cutter system and more! It can also run on Mac.
-It encompasses everything progressive. You can basically call it
-a brand new genre of YTPMV.
-* Will I need to do anything to upgrade?
-* YTPMV+ is still in its development stages, but we have a stable build
-ready for preview release. You are able to use Sony Vegas along with
-this build using the included YTPMVegasAdaptor.exe in the release.
-YTPMV+ program github 
-* a piece of paper signed by youtube star Kem Himself
-* access to YTPMV+ once it is released github
-* special Brony edition YTPMV+ program, complete with the perfect samples and sounds you will find within the FiM show
-* special King Of The Hill edition YTPMV+ program, complete with the perfect samples and sounds found within KotH, and even an "auto-poop" button that will create a KotH poop your fans will love
-* access to the beta test of YTPMV+
+# ToolpostGenerator
+This is an open source recreation of the software that powered ToolpostBot since February 2018, up until some time in early 2019. It used to run off a mess of Batch scripts, PHP, webservers, and Node.js. I won't put anyone through that, so here's a completely Node.js rewrite based off the ToolpostBot Discord bot source.
+
+**Designed for Windows.**
+
+### Dependencies
+- Knowledge on how to setup Node.js and all the other shit needed to make this work.
+- ffmpeg and ffprobe in your %PATH% or in the directory of `toolpostgenerator.js`. You can get these executables from https://ffmpeg.org/download.html.
+
+### How to Use
+- You need Node.js from https://nodejs.org.
+- In the same directory as `toolpostgenerator.js`, you need a source MP4 as the video for your generation. Call this `source.mp4`. For best results, don't make it too long.
+- Then again in the same directory as `toolpostgenerator.js`, you need a `sourcemp3` directory, with ideally as many MP3 files as possible, for maximum randomness. The script will ignore MP3s shorter than the video source.
+- Run the batch file `toolpostgenerator.bat` (or run command `node toolpostgenerator.js`) to see it work.
+
+### Contributions
+Please feel free to make contributions! Refactors, bug fixes, grammar correcting, anything! Please do not judge my code too much!
+
+### MIT License
+Copyright (c) 2019 ToolpostBot
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ toolpostgenerator.js
@@ -0,0 +1,147 @@
+/* 
+    View LICENSE for license information (MIT License)
+    Copyright (c) 2019 ToolpostBot
+
+    Please don't judge my coding habits too much :(
+*/
+
+const { exec } = require('child_process');
+const fs = require('fs');
+
+async function main() {
+    // Do basic checks and get file information
+    try {
+        await ffmpeg();
+    } catch (err) {
+        console.error('Please install FFMPEG and FFPROBE into the working directory or into your %PATH%.\nFFMPEG can be downloaded at: https://ffmpeg.org/download.html');
+        return false;
+    }
+
+    if (!fs.existsSync('source.mp4')) {
+        console.error('Please place a source video named "source.mp4" into this directory to be used as the video in your generation.');
+        return false;
+    }
+
+    let videoLength;
+    try {
+        videoLength = await getVideoLength();
+    } catch(err) {
+        console.error(`There was a problem with your video source:\n\n${err.message}`);
+        return false;
+    }
+
+    if (!fs.existsSync('sourcemp3/')) fs.mkdirSync('sourcemp3');
+
+    let sources = fs.readdirSync('sourcemp3/');
+    if (sources.length == 0) {
+        console.error('Please add at least one MP3 into "sourcemp3\\".');
+        return false;
+    }
+
+    if (!fs.existsSync('temp\\')) fs.mkdirSync('temp');
+
+    // Find suitable source
+    let sourceLength = 0, source;
+    while (!sourceLength) {
+        let sourceIndex = Math.floor(Math.random() * sources.length);
+        source = sources[sourceIndex];
+        let currentSourceLength;
+
+        try {
+            currentSourceLength = await getSourceLength(source); 
+        } catch(err) {
+            console.error(`There was a problem with the source "sourcemp3\\${source}":\n\n${err.message}`);
+            return false;
+        }
+
+        // Remove too short sources from consideration
+        if (currentSourceLength > videoLength) sourceLength = await getSourceLength(source);
+        else sources.splice(0, 1);
+
+        // If out of sources, throw error
+        if (sources.length == 0) {
+            console.error(`Please add at least one MP3 that is longer than the length of source.mp4 (${videoLength}) into "sourcemp3\\".`);
+            return false;
+        }
+    }
+
+    let sourceStartSecond = Math.floor(Math.random() * (sourceLength - videoLength));
+
+    // Generate I guess
+    let filename;
+    try {
+        filename = await generateVideo(source, sourceStartSecond, videoLength);
+    } catch(err) {
+        console.error(`Something went wrong during generation:\n\n${err.message}`);
+        return false;
+    }
+
+    console.log(`Source MP3: ${source}`);
+    console.log(`Starting point: ${sourceStartSecond} seconds`);
+    console.log(`Filename: ${filename}`);
+
+    fs.renameSync(`temp\\${filename}`, filename);
+
+    return true;
+}
+
+function getSourceLength(file) {
+    return new Promise((resolve, reject) => {
+        exec(`ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "sourcemp3\\${file}"`, (err, stdout, stderr) => {
+            if (err) {
+                reject(err);
+            } else if (stderr) {
+                reject(stderr);
+            } else {
+                resolve(Math.floor(stdout));
+            }
+        });
+    });
+}
+
+function getVideoLength() {
+    return new Promise((resolve, reject) => {
+        exec('ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 source.mp4', (err, stdout, stderr) => {
+            if (err) {
+                reject(err);
+            } else if (stderr) {
+                reject(stderr);
+            } else {
+                resolve(Math.floor(stdout));
+            }
+        });
+    });
+}
+
+function generateVideo(file, time, videoLength) {
+    return new Promise((resolve, reject) => {
+        let d = new Date();
+        let filename = `gen_${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}_${d.toTimeString().substr(0,8).replace(/:/g,'')}.mp4`;
+        exec(`ffmpeg -i source.mp4 -ss ${time} -i "sourcemp3\\${file}" -map 0:v -map 1:a -c:v h264 -c:a copy -t ${videoLength} temp\\${filename} -v error -y`, (err, stdout, stderr) => {
+            if (err) {
+                reject(err);
+            } else if (stderr) {
+                reject(stderr);
+            } else {
+                resolve(filename);
+            }
+        });
+    });
+}
+
+// This seems a little hacky
+function ffmpeg() {
+    return new Promise((resolve, reject) => {
+        exec(`ffmpeg -version`, (err, stdout, stderr) => {
+            if (err) reject(false);
+            else {
+                exec(`ffprobe -version`, (err, stdout, stderr) => {
+                    if (err) reject(false);
+                    else resolve(true);
+                });
+            }
+        });
+    });
+}
+
+main();
